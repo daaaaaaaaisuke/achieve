b825b35f573118797e13e59efb03258b861b2f69
Daisuke Tokui <tvml.725@gmail.com>
2016-08-30 11:20:31 +0000

    update READEME.rdoc

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,21 @@
 Rails.application.routes.draw do
-  resources :blogs
+
+
+
+
+  resources :blogs, only: [:index, :new, :create, :edit, :update, :destroy] do
+    collection do
+      post :confirm
+    end
+  end
+  
+  resources :contacts, only: [:new, :create] do
+   collection do 
+     post :confirm
+   end
+ end
+ 
+  root 'top#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
